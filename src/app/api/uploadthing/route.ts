2189c5264b5364423ba9610e2c30978b07d8d122
Aigars Grenins <aigars.grenins@gmail.com>
2025-07-01 14:39:49 +0300

    fix(uploadthing): update to modern API for route handler

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,10 +1,8 @@
-import { createRouteHandler } from "@uploadthing/next";
+import { createNextRouteHandler } from "uploadthing/next";
+ 
 import { ourFileRouter } from "./core";
-
-export const { GET, POST } = createRouteHandler({
+ 
+// Export routes for Next App Router
+export const { GET, POST } = createNextRouteHandler({
   router: ourFileRouter,
-  config: {
-    uploadthingId: process.env.UPLOADTHING_APP_ID,
-    uploadthingSecret: process.env.UPLOADTHING_SECRET,
-  },
 });
